3fbddc07c7b205a1e618cc0521489ffa37c1bc5c
Robin <robin.werb@web.de>
2025-10-01 00:37:37 +0200

    TDD - green: char_to_num (Zur Berechnung der Größe des Parkplatzes)

--- src/schiebeparkplatz.py
+++ src/schiebeparkplatz.py
@@ -8,3 +8,8 @@ def read_first_line(path: str) -> str:
         line = f.readline().strip()
         tokens = line.split()
         return tokens[1]
+
+
+def char_to_num(char: str) -> int:
+    char = char.upper()
+    return ord(char) - ord("A") + 1

31feec3713d24019c46e0cdf12499043b5cdb81c
Robin <robin.werb@web.de>
2025-10-01 00:21:44 +0200

    TDD - red: read_first_line (Wir brauchen nur den zweiten Buchstaben/ String)

--- tests/tests_schiebeparkplatz.py
+++ tests/tests_schiebeparkplatz.py
@@ -1,5 +1,6 @@
 import pytest
 from src.schiebeparkplatz import read_in_parkplatz
+from src.schiebeparkplatz import read_first_line
 
 def test_read_in_parkplatz(tmp_path):
 
@@ -11,8 +12,12 @@ def test_read_in_parkplatz(tmp_path):
     assert result == ["Parkplatz A", "Parkplatz B"]
 
 
-def test_read_first_line(tmp_path):
+
+def test_read_in_first_line(tmp_path):
+
     test_file = tmp_path / "test.txt"
     test_file.write_text("foo bar baz\nzweite zeile\n")
-    result = read_first_line(str(test_file))
-    assert result == "foo bar baz"
+
+    result = read_in_first_line(str(test_file))
+
+    assert result == "bar"
